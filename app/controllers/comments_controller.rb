da41d92c5ab0eb3bd3ab092478d27367447dc7f9
afiuchida <uchidakaede0928@rakuten.jp>
2021-03-18 19:18:49 +0900

    second_commit

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,17 +1,16 @@
 class CommentsController < ApplicationController
   def create
-    @comment = Comment.new(comment_params)
-    if @comment.save
+   @comment = Comment.new(comment_params)
+  if @comment.save
       redirect_to prototype_path(@comment.prototype)
     else
-        render "prototypes/show"
-        @prototype = @comment.prototype
-        @comments = @prototype.comments
+      @prototype = @comment.prototype
+      @comments = @prototype.comments
+      render "prototypes/show"
     end
   end
-
-    private
-    def comment_params
-     params[:comment].permit(:text, :prototype, :user).merge(user_id: current_user.id,  prototype_id: params[:prototype_id])
-    end
- end
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
+  end
+end
