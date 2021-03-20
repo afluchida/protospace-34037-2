c086df54040fd5372405b11bbb571cab45c341f8
afiuchida <uchidakaede0928@rakuten.jp>
2021-03-20 11:43:52 +0900

    commit

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,13 +1,10 @@
 class Prototype < ApplicationRecord
-  validates :title, presence: true
-         validates :catch_copy, presence: true
-         validates :concept, presence: true
-         validates :image, presence: true, unless: :was_attached?
   belongs_to :user
-  has_many :comments
-
   has_one_attached :image
-  def was_attached?
-    self.image.attached?
-  end
+  has_many :comments, dependent: :destroy
+
+  validates :title, presence: true
+  validates :catch_copy, presence: true
+  validates :concept, presence: true  
+  validates :image, presence: true
 end
