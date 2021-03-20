b8457db22d0ad9eb3f45c7799a4db23c94e637fb
afiuchida <uchidakaede0928@rakuten.jp>
2021-03-20 11:07:49 +0900

    commit

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
   def create
-   @comment = Comment.new(comment_params)
-  if @comment.save
+    @comment = Comment.new(comment_params)
+    if @comment.save
       redirect_to prototype_path(@comment.prototype)
     else
       @prototype = @comment.prototype
@@ -9,7 +9,9 @@ class CommentsController < ApplicationController
       render "prototypes/show"
     end
   end
+
   private
+
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
   end
--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -16,41 +16,40 @@ class PrototypesController < ApplicationController
     render :new
   end
  end
- 
  def show
-  @prototype = Prototype.find(params[:id])
   @comment = Comment.new
-  @comments = Comment.find_by(id:params[:id], prototype_id: params[:prototype_id])     
- @comments = @prototype.comments.includes(:user)
- end
-
-def edit
-  @prototype = Prototype.find(params[:id])
+  @comments = @prototype.comments
 end
 
+def edit
+end
+
+
 def update
-  prototype = Prototype.find(params[:id])
-  if prototype.update(prototype_params)
-    redirect_to prototype_path
+  if @prototype.update(prototype_params)
+    redirect_to prototype_path(@prototype)
   else
     render :edit
   end
 end
 
 def destroy
-  prototype = Prototype.find(params[:id])
-  if prototype.destroy
+  if @prototype.destroy
     redirect_to root_path
-  end    
+  else
+    redirect_to root_path
+  end
 end
-
 private
+
 def prototype_params
   params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
 end
+
 def set_prototype
   @prototype = Prototype.find(params[:id])
 end
+
 def contributor_confirmation
   redirect_to root_path unless current_user == @prototype.user
 end
